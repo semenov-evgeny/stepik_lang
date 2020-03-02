fc1ff1afa1476e7ba587c0e1cdcdc1c03f142776
xfail <xfail@localhost.localdomain>
2020-03-02 16:04:32 +0300

    Исправлено задание и выгружено для проверки

--- test_items.py
+++ test_items.py
@@ -4,9 +4,8 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_should_see_add_basket_button(browser):
     browser.get(link)
-    time.sleep(10)
+    #time.sleep(30)
     button_name = browser.find_element_by_css_selector("#add_to_basket_form")
     assert button_name.is_displayed(), \
-    'Кнопка добавить в корзину не найдена'
-    assert button_name.text == "Ajouter au panier", \
-    f'Кнопка добавить в корзину отображается как {button_name.text}'
+        'Кнопка добавить в корзину не найдена'
+    
